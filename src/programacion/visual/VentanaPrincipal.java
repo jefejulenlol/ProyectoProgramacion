8edd0a0e249d108e48dab1694fbe7a1a15cb500d
jefejulenlol <jefejulenlol@gmail.com>
2021-01-16 06:19:19 +0100

    Acabada la clase VentanaPrincipal, su Swing e interacciones con la base de datos en los action listeners de los botones

--- src/programacion/visual/VentanaPrincipal.java
+++ src/programacion/visual/VentanaPrincipal.java
@@ -1,19 +1,21 @@
 package programacion.visual;
 /**
  * 
- * @author Julen Peralta
- * @version 1.0
- * Se que el Layout deberia ser el de Gridlayout pero no he conseguido que quedara bien usando ese layout asi que empleo el Absolute
+ * @author Julen
+ *@version final
  */
+
+//SOY CONSCIENTE DE QUE DEBERÍA USAR EL GRIDLAYOUT Y NO EL ABSOLUTE, PERO PERDÍA MUCHA MOVILIDAD PARA COLOCAR LOS COMPONENTES COMO QUERÍA Y ERA MUY INCOMODO
 
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 
-import conexionesBasesDeDatos.ConexionLogin;
+import acceso.ficheros.EscrituraYLecturaBD;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
@@ -22,8 +24,8 @@ import java.awt.event.ActionEvent;
 public class VentanaPrincipal {
 
 	private JFrame frmTiendaDeRopa;
-	private JTextField textField;
-	private JPasswordField passwordField;
+	private JTextField introducirUsuario;
+	private JPasswordField introducirContrasenya;
 	private JButton btnLogin;
 
 	/**
@@ -55,7 +57,7 @@ public class VentanaPrincipal {
 	private void initialize() {
 		frmTiendaDeRopa = new JFrame();
 		frmTiendaDeRopa.setTitle("Tienda de Ropa Julen's");
-		frmTiendaDeRopa.setBounds(100, 100, 595, 406);
+		frmTiendaDeRopa.setBounds(100, 100, 625, 384);
 		frmTiendaDeRopa.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmTiendaDeRopa.getContentPane().setLayout(null);
 		
@@ -67,31 +69,70 @@ public class VentanaPrincipal {
 		lblUsuario.setBounds(75, 133, 56, 16);
 		frmTiendaDeRopa.getContentPane().add(lblUsuario);
 		
-		JLabel lblPassword = new JLabel("Password:");
-		lblPassword.setBounds(75, 173, 65, 16);
-		frmTiendaDeRopa.getContentPane().add(lblPassword);
+		JLabel lblContrasenya = new JLabel("Password:");
+		lblContrasenya.setBounds(75, 173, 65, 16);
+		frmTiendaDeRopa.getContentPane().add(lblContrasenya);
 		
-		textField = new JTextField();
-		textField.setBounds(158, 130, 116, 22);
-		frmTiendaDeRopa.getContentPane().add(textField);
-		textField.setColumns(10);
+		introducirUsuario = new JTextField();
+		introducirUsuario.setBounds(158, 130, 116, 22);
+		frmTiendaDeRopa.getContentPane().add(introducirUsuario);
+		introducirUsuario.setColumns(10);
 		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(158, 170, 116, 22);
-		frmTiendaDeRopa.getContentPane().add(passwordField);
+		introducirContrasenya = new JPasswordField();
+		introducirContrasenya.setBounds(158, 170, 116, 22);
+		frmTiendaDeRopa.getContentPane().add(introducirContrasenya);
 		
+		/**
+		 *Al presionar el boton login e introducir las credenciales se comprueban estas con las guardadas en la base de datos y se le permite o no el acceso
+		 */
 		btnLogin = new JButton("Login");
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				
 			//Establezco la  conexion a la base de datos al presionar el boton de Login para comprobar credenciales	
-			ConexionLogin conexion = new ConexionLogin();
-			conexion.ConexionBDLogin();
-	
-			
-			}
+				String usuario = introducirUsuario.getText();
+				String pass = new String(introducirContrasenya.getPassword());
+				EscrituraYLecturaBD conexion = new EscrituraYLecturaBD();
+				
+				if(conexion.selectLoginData(usuario,pass)==true) {
+					frmTiendaDeRopa.setVisible(false);
+				}else {
+					JOptionPane.showMessageDialog(null, "Credenciales no validas");
+				}
+
+				
+			}	
 		});
 		btnLogin.setBounds(240, 244, 97, 25);
 		frmTiendaDeRopa.getContentPane().add(btnLogin);
+		
+		/**
+		 *Abre la ventana de registro y cierra la ventanaprincipal
+		 */
+		JButton btnRegistrarse = new JButton("Registrarse");
+		btnRegistrarse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				VentanaRegistro ventanaRegistro = new VentanaRegistro();
+				ventanaRegistro.mostrarVentanaRegistro();
+				frmTiendaDeRopa.setVisible(false);
+				
+			}
+		});
+		btnRegistrarse.setBounds(240, 292, 106, 25);
+		frmTiendaDeRopa.getContentPane().add(btnRegistrarse);
+	}
+	
+	public void mostrarVentanaPrincipal(){
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaPrincipal window = new VentanaPrincipal();
+					window.frmTiendaDeRopa.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 }
