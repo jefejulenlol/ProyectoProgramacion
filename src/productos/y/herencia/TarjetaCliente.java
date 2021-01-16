7fa2fe422d8b42174dcde2fd9be4772065374d74
jefejulenlol <jefejulenlol@gmail.com>
2021-01-16 06:35:26 +0100

    Acabada la clase TarjetaCliente

--- src/productos/y/herencia/TarjetaCliente.java
+++ src/productos/y/herencia/TarjetaCliente.java
@@ -1,5 +1,40 @@
 package productos.y.herencia;
 
-public class TarjetaCliente {
+/**
+ * 
+ * @author Julen
+ *@version final
+ */
+
+public class TarjetaCliente extends Tarjeta{
+	
+	private String descuento = "10%";
+
+	/**
+	 * getter del descuento
+	 * @return descuento , devuelve el porcentaje a descontar
+	 *
+	 */
+	public String getDescuento() {
+		return descuento;
+	}
+
+	/**
+	 * setter de descuento
+	 * @param descuento , asigna el descuento
+	 *
+	 */
+	public void setDescuento(String descuento) {
+		this.descuento = descuento;
+	}
+	
+	/**
+	 * un metodo por el que paga con descuento los articulos comprados
+	 * @return devuelve un string confirmando el pago "Pagado con un descuento del " + descuento
+	 *
+	 */
+	public String pagar() {
+		return "Pagado con un descuento del " + descuento;
+	}
 
 }
