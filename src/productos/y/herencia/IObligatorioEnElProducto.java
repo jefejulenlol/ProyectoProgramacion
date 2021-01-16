729258b31421191b179edbfa19e61d1ed74551b4
jefejulenlol <jefejulenlol@gmail.com>
2021-01-16 06:31:29 +0100

    Acabada la Interfaz IObligatorioEnElProducto

--- src/productos/y/herencia/IObligatorioEnElProducto.java
+++ src/productos/y/herencia/IObligatorioEnElProducto.java
@@ -1,5 +1,25 @@
 package productos.y.herencia;
 
+/**
+ * 
+ * @author Julen
+ *@version final
+ */
+
 public interface IObligatorioEnElProducto {
+		
+	int getTalla();
+	String getColor();
+	int getPrecio();
+	String getTipoProducto();
+	String getTemporadaDeRopa();
+	String getSexo();
+	
+	void setTalla(int x);
+	void setColor(String x);
+	void setPrecio(int x);
+	void setTipoProducto(String x);
+	void setTemporadaDeRopa(String x);
+	void setSexo(String x);
 
 }
