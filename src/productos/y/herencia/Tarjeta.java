d542e557f4e574b1587da68f05a2bea311e05fcb
jefejulenlol <jefejulenlol@gmail.com>
2021-01-16 06:34:19 +0100

    Acabada la clase Tarjeta

--- src/productos/y/herencia/Tarjeta.java
+++ src/productos/y/herencia/Tarjeta.java
@@ -1,5 +1,20 @@
 package productos.y.herencia;
 
+/**
+ * 
+ * @author Julen
+ *@version final
+ */
+
 public class Tarjeta {
 
+	/**
+	 * Un metodo para pagar la ropa
+	 * @return devuelve un String que pone "Pagado sin descuento"
+	 *
+	 */
+	public String pagar() {
+		return "Pagado sin descuento";
+	}
+	
 }
